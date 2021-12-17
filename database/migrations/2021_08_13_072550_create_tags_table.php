57ebadef2f3140dd5fbe237a0eac0a773dbb38e2
ktwj <asahi_1282@yahoo.co.jp>
2021-12-17 18:35:12 +0900

    Add Procfile, varcher length, force https

--- database/migrations/2021_08_13_072550_create_tags_table.php
+++ database/migrations/2021_08_13_072550_create_tags_table.php
@@ -14,7 +14,7 @@ class CreateTagsTable extends Migration
     public function up()
     {
         Schema::create('tags', function (Blueprint $table) {
-            $table->unsignedBigInteger('id');
+            $table->unsignedBigInteger('id',true);
             $table->string('name');
             $table->unsignedBigInteger('user_id');
             $table->softDeletes();
